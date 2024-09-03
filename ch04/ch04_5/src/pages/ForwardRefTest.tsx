27be065af1190ac60b907821749734c9951a839d
smathj <smathj@naver.com>
2024-09-03 18:50:41 +0900

    4.5 forwardRef 은 쓰겠는데 , useImperativeHandle 은 굳이 쓰는게 더 더러운것 같다

--- ch04/ch04_5/src/pages/ForwardRefTest.tsx
+++ ch04/ch04_5/src/pages/ForwardRefTest.tsx
@@ -3,11 +3,28 @@ import {Title} from '../components'
 import {Input, Button} from '../theme/daisyui'
 
 export default function ForwardRefTest() {
-  // prettier-ignore
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const getValue = useCallback(() => {
+    alert(`input value: ${inputRef.current?.value}`)
+  }, [])
+
+  useEffect(() => {
+    inputRef.current?.focus()
+  }, [])
+
   return (
     <section className="mt-4">
       <Title>ForwardRefTest</Title>
-
+      <div className="flex justify-center mt-4">
+        <div className="flex flex-col w-1/3 p-2">
+          <Input ref={inputRef} className="input input-primary" />
+          <Button onClick={getValue} className="mt-4 btn-primary">
+            GET VALUE
+          </Button>
+        </div>
+      </div>
+      <input />
     </section>
   )
 }
